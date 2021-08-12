16e28e5e86048feb4c2142d4935a7422e8f2bdda
Kevin Funez <kevin.funez315@gmail.com>
2021-08-12 15:10:43 -0700

    intial commit
    Co-authored-by: Chynna Lew <chynnalew@yahoo.com>

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("#triangle").submit(function(event) {
+    event.preventDefault();
+    const sideOne = parseInt($("input#sideOne").val());
+    const sideTwo = parseInt($("input#sideTwo").val());
+    const sideThree = parseInt($("input#sideThree").val());
+    
+    if (sideOne >= (sideTwo + sideThree) || sideTwo >= (sideOne + sideThree) || sideThree >= (sideOne + sideTwo)) {
+      $("span#result").text("not a  Triangle") 
+    } else if (sideOne === sideTwo && sideTwo === sideThree) {
+      $("span#result").text("Equilateral Triangle")
+    } else if (sideOne === sideTwo || sideTwo === sideThree || sideThree === sideOne) {
+      $("span#result").text("Isosceles Triangle")
+    } else if (sideOne !== sideTwo && sideTwo !== sideThree && sideThree !== sideOne){
+      $("span#result").text("Scalene Triangle") 
+    } else
+    $("span#result").text("Where are you???")
+  });
+});
